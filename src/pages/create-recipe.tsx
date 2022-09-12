20c0b8b03a33b1a37706098bd6130b49be9b4eeb
Samuel Harris <sw.gleaves@gmail.com>
2022-09-11 23:12:45 -0400

    slightly better looking ui on create page. . . slightly

--- src/pages/create-recipe.tsx
+++ src/pages/create-recipe.tsx
@@ -1,56 +1,55 @@
 
 import { FC, useState } from "react";
-import { Recipe, RecipeStep } from "../schema/recipe.schema";
 import { trpc } from "../utils/trpc";
 
-interface StepParams {
-  recipeId: number,
-  stepNumber: number
-}
+// interface StepParams {
+//   recipeId: number,
+//   stepNumber: number
+// }
 
-const CreateRecipeStep:FC<StepParams> = (p) => {
-  const [step, setStep] = useState<RecipeStep>({
-    recipeId: p.recipeId,
-    text: "Step" + p.stepNumber,
-    stepNumber: p.stepNumber
-  });
-  const postRecipeStep = trpc.useMutation(["recipes.postRecipeStep"]);
+// const CreateRecipeStep:FC<StepParams> = (p) => {
+//   const [step, setStep] = useState<RecipeStep>({
+//     recipeId: p.recipeId,
+//     text: "Step" + p.stepNumber,
+//     stepNumber: p.stepNumber
+//   });
+//   const postRecipeStep = trpc.useMutation(["recipes.postRecipeStep"]);
 
-  return (
-  <div className="pt-6">
-              <form
-                className="flex gap-2"
-                onSubmit={(event) => {
-                  event.preventDefault();
-                  postRecipeStep.mutate(step);
-                }}
-              >
-                <input
-                  type="text"
-                  value={step.title}
-                  placeholder="Give the step an optional title"
-                  onChange={(event) => setStep({...step, title: event.target.value})}
-                  className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
-                />
-                <input
-                  type="text"
-                  value={step.text}
-                  placeholder="Add a step..."
-                  onChange={(event) => setStep({...step, text: event.target.value})}
-                  className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
-                />
-                <button
-                  type="submit"
-                  className="p-2 rounded-md border-2 border-zinc-800 focus:outline-none"
-                >
-                  Save Step
-                </button>
-              </form>
-            </div>
-  );
-}
+//   return (
+//   <div className="pt-6">
+//               <form
+//                 className="flex gap-2"
+//                 onSubmit={(event) => {
+//                   event.preventDefault();
+//                   postRecipeStep.mutate(step);
+//                 }}
+//               >
+//                 <input
+//                   type="text"
+//                   value={step.title}
+//                   placeholder="Give the step an optional title"
+//                   onChange={(event) => setStep({...step, title: event.target.value})}
+//                   className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
+//                 />
+//                 <input
+//                   type="text"
+//                   value={step.text}
+//                   placeholder="Add a step..."
+//                   onChange={(event) => setStep({...step, text: event.target.value})}
+//                   className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
+//                 />
+//                 <button
+//                   type="submit"
+//                   className="p-2 rounded-md border-2 border-zinc-800 focus:outline-none"
+//                 >
+//                   Save Step
+//                 </button>
+//               </form>
+//             </div>
+//   );
+// }
 
-const CreateRecipe = () => {
+const CreateRecipe:FC = () => {
   const [title, setTitle] = useState("");
   const [steps, setSteps] = useState<string[]>([""]); // temp way until using form hook of some sort
   const ctx = trpc.useContext();
@@ -75,7 +74,7 @@ const CreateRecipe = () => {
   return(
     <main className="flex flex-col items-center">
       <h1>Add a recipe</h1>
-    <div className="pt-6">
+    <div className="pt-6 w-screen max-w-prose">
               <form
                 className="flex flex-col gap-2"
                 onSubmit={(event) => {
@@ -86,43 +85,52 @@ const CreateRecipe = () => {
                   });
                 }}
               >
-                <input
-                  type="text"
+                <div className=" bg-gray-700 flex flex-col p-2">
+                <textarea
+                  className="bg-inherit resize-none focus:outline-none"
                   value={title}
+                  rows={1}
                   required
                   placeholder="Name your recipe..."
-                  maxLength={100}
+                  maxLength={60}
                   onChange={(event) => setTitle(event.target.value)}
-                  className="bg-inherit border-violet-500 border-2 rounded-lg h-10"
                 />
-                {steps.map((step, index) => {
-                  return(
-                  <textarea 
+                <textarea
+                  className="bg-inherit resize-none border-gray-500 border-t-2 focus:outline-none pt-2"
+                  value={title}
+                  rows={3}
+                  placeholder="Give an optional description"
+                  maxLength={250}
+                  onChange={(event) => setTitle(event.target.value)}
+                />
+                {steps.map((step, index) =>
+
+                  <div>
+
+                  <textarea
+                  className="w-full bg-slate-600 rounded-md resize-none border-gray-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   key={index}
-                  rows={5} 
-                  cols={45} 
                   minLength={1}
                   maxLength={400}
                   value={step}
-                  placeholder="What comes next?"
+                  placeholder={"Step " + (index + 1)}
                   onChange={(event) => {
                     event.preventDefault();
                     setSteps(steps.map((s, i) => (i === index) ? event.target.value : s ));
-                  }
-                  } 
-                  className="bg-inherit border-cyan-900 border-2 rounded-lg"
-                  />
-                );})}
+                  }} 
+                />
+                </div>)}
+                </div>
                 
                 <button
-                  className="bg-inherit border-cyan-900 border-4 rounded-lg "
+                  className=" bg-gray-600 text-base font-bold"
                   onClick={() => setSteps([...steps, ""])}
                 >
-                  Add Another Step
+                  Add Step
                 </button>
                 <button
+                  className="p-2 bg-green-700 text-base font-bold focus:outline-none"
                   type="submit"
-                  className="btn p-2 btn-accent rounded-md border-2 border-zinc-800 focus:outline-none"
                 >
                   Save
                 </button>
